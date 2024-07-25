4bcb36dce87e59674740fb0bba2304fe3e8b9f75
Florant Hauguel <hauguel@abyssecorp.com>
2024-07-25 09:51:19 +0200

    initial commit 2

--- src/tasks/taskColumn/TaskColumn.tsx
+++ src/tasks/taskColumn/TaskColumn.tsx
@@ -3,8 +3,8 @@ import TaskItem from '../taskItem/TaskItem';
 
 function TaskColumn({ title, tasks }) {
     return (
-    <div className='flex-1 p-2 border border-slate-300 flex flex-col gap-4'>
-      <h2 className='text-xl text-blue-500 font-normal'>{title}</h2>
+    <div className='flex-1 p-2 border rounded border-blue-300 flex flex-col gap-4'>
+      <h2 className='text-xl text-blue-500 font-bold'>{title}</h2>
        {tasks.map((task, index) => <TaskItem key={index} status={title} title={task.title} taskStatus={task.status} description={task.description} tr={task.tr} ti={task.ti}/>)} 
     </div>
   )
